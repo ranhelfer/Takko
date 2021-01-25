f38417982c75684a4d46846d5b64ffde6a8a0be9
rhalfer <rhalfer@shutterfly.com>
2021-01-25 11:58:35 +0200

    Initial Commit

---
+++ YelpDemo/YelpDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  YelpDemoApp.swift
+//  YelpDemo
+//
+//  Created by rhalfer on 25/01/2021.
+//
+
+import SwiftUI
+
+@main
+struct YelpDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
